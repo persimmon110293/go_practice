4d71f2b1d9d67f0dbea9f703936ad1173171f1b9
Shohei Kakimoto <shohei.11293@gmail.com>
2024-02-12 17:39:40 +0900

    feature/#1 add: added gin and air for hot reload

--- go/src/server.go
+++ go/src/server.go
@@ -1,15 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
-}
+import "github.com/gin-gonic/gin"
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "レスポンスっす",
+		})
+	})
+
+	r.Run()
 }
